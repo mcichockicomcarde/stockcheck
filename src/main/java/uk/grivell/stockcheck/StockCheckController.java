f83438bc4427b9db7a8863c6af5d9efd45a5eff6
Marcin Cichocki <marcin.cichocki@comcarde.com>
2020-02-11 18:03:34 +0000

    find test

--- src/main/java/uk/grivell/stockcheck/StockCheckController.java
+++ src/main/java/uk/grivell/stockcheck/StockCheckController.java
@@ -1,11 +1,13 @@
 package uk.grivell.stockcheck;
 
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException.NotFound;
 import uk.grivell.stockcheck.persistence.*;
 import uk.grivell.stockcheck.dto.StockCheckResult;
 
@@ -41,5 +43,21 @@ public class StockCheckController {
         return new StockCheckResult(stockCheckRecommendations);
     }
 
+    @GetMapping("/prepare")
+    @ResponseBody
+    public StockCheckRecommendation prepare(){
+        final StockCheckRecommendation explosion = new StockCheckRecommendationBuilder()
+            .withName("prepare")
+            .withComments("comments")
+            .withCurrentStock(3)
+            .withOrderQuantity(1)
+            .build();
+        return stockCheckRecommendationRepository.save(explosion);
+    }
 
+    @GetMapping("/explode")
+    @ResponseBody
+    public StockCheckRecommendation explode(){
+        return stockCheckRecommendationRepository.findById("1").orElseThrow(() -> new RuntimeException("whatever"));
+    }
 }
